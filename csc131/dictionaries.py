bbe1d32e6c17ef361ff92ea58ac68a5be579434a
cjs61 <cody6324@live.missouristate.edu>
2018-01-26 13:13:36 -0600

    The initial import of CSC 131 package.

---
+++ csc131/dictionaries.py
@@ -0,0 +1,7 @@
+def get_personal_data() -> dict:
+    """
+
+    :return: Returns a dictionary of personal information
+    """
+    personal-data = {"Name": "Jim", "Role": "Student"}
+    return personal_data
