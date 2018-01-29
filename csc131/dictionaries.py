ffd9dcc3c8a21e91af5dbd53a1e901649d1816db
cjs61 <cody6324@live.missouristate.edu>
2018-01-29 13:07:39 -0600

    The initial import of CSC 131 package.

--- csc131/dictionaries.py
+++ csc131/dictionaries.py
@@ -1,3 +1,6 @@
+import string
+
+
 def get_personal_data() -> dict:
     """
     :return: Returns a dictionary of personal information
@@ -9,10 +12,16 @@ def get_personal_data() -> dict:
 def main() -> int:
     constructor_or_default_dict = dict()
     print(constructor_or_default_dict)
+
     initialized_dict = dict([('Name', 'Jim'), ('A_Role', 'Joker')])
     print(initialized_dict)
+
     simple_initialized_list = dict(Name='Jim', A_Role='Teacher')
+    simple_initialized_list['A_Role'] = 'Gym Teacher'   #Assigns a new value to a dict that already exists
     print(simple_initialized_list)
+    my_comprehension = {x: x**2 for x in range(5)}   #Referred to as a dict comprehension
+    print(my_comprehension)
+
     return 0
 
 
